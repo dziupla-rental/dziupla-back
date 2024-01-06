154f7650ca6bbfd3a689fc2436d4da42b859b24f
Jakub Gazda <kgazda53@gmail.com>
2024-01-06 16:05:22 +0100

    create CompanyInfo

---
+++ src/main/java/shop/dziupla/spring/login/models/CompanyInfo.java
@@ -0,0 +1,35 @@
+package shop.dziupla.spring.login.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "company_info",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = "NIP")
+        })
+public class CompanyInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 300)
+    String name;
+
+    @Size(min = 10, max = 10)
+    int NIP;
+
+    public CompanyInfo() {}
+    public CompanyInfo(String name, int NIP) {
+        this.name = name;
+        this.NIP = NIP;
+    }
+
+    public void setId(Long id) { this.id = id; }
+    public Long getId() { return id; }
+    public void setName(String name) { this.name = name; }
+    public String getName() { return name; }
+    public void setNIP(int NIP) { this.NIP = NIP; }
+    public int getNIP() { return NIP; }
+}
+
