b83bda431f8dee5893e28356cd8bc39ff8be44ca
Jakub Gazda <kgazda53@gmail.com>
2024-01-14 17:32:40 +0100

    add not null constraints

--- src/main/java/shop/dziupla/spring/login/models/Rental.java
+++ src/main/java/shop/dziupla/spring/login/models/Rental.java
@@ -1,7 +1,7 @@
 package shop.dziupla.spring.login.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
@@ -15,23 +15,26 @@ public class Rental {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "car_id", referencedColumnName = "id")
     private Car car;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "client_id", referencedColumnName = "id")
     private Client client;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "origin_office_id", referencedColumnName = "id")
     private Office originOffice;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "destination_office_id", referencedColumnName = "id")
     private Office destinationOffice;
 
-    @NotBlank
     private Long protocolId;
 
     private Date startDate;
