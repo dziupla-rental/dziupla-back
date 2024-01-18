14614141a4da9920784ff7399b8429b451e93e42
Jakub Gazda <kgazda53@gmail.com>
2024-01-18 23:33:42 +0100

    create mappers for Client and CompanyInfo

---
+++ src/main/java/shop/dziupla/spring/login/mappers/CompanyInfoMapper.java
@@ -0,0 +1,13 @@
+package shop.dziupla.spring.login.mappers;
+
+import org.mapstruct.Mapper;
+import shop.dziupla.spring.login.models.DAO.CompanyInfo;
+import shop.dziupla.spring.login.payload.response.CompanyInfoDTO;
+
+@Mapper(componentModel = "spring")
+public interface CompanyInfoMapper {
+
+    CompanyInfoDTO carToCarDto(CompanyInfo entity);
+
+    CompanyInfo companyInfoDTOToCompanyInfo(CompanyInfoDTO dto);
+}
