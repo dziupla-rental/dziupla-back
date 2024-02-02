6c3aee00eb61c9e3512af4e07a169d576ad6ea1d
Jakub Gazda <kgazda53@gmail.com>
2024-02-02 19:44:41 +0100

    handles REST methods

--- src/main/java/shop/dziupla/spring/login/controllers/ClientController.java
+++ src/main/java/shop/dziupla/spring/login/controllers/ClientController.java
@@ -97,17 +97,6 @@ public class ClientController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
-//    @GetMapping("/details")
-//    public RespClientDetails getDetails() {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        RespClientDetails response = new RespClientDetails();
-//        String username = auth.getName();
-//        // TODO wyszukać dane dla klienta przypisanego do danego użytkownika
-//        response.setFirst_name(username); // when testing in postman you might have to singout and signin again for this to work
-//
-//        return response;
-//    }
 }
 
 
