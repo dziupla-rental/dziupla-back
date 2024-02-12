c6b3b92bd9ad382b31eeaedf5683b09fb25af709
Wiktoria Kołodziej <wiktoria.kolodziej@gmail.com>
2024-02-12 15:03:52 +0100

    statistics

--- src/main/java/shop/dziupla/spring/login/payload/response/StatisticsDTO.java
+++ src/main/java/shop/dziupla/spring/login/payload/response/StatisticsDTO.java
@@ -1,70 +1,70 @@
 package shop.dziupla.spring.login.payload.response;
 
 public class StatisticsDTO {
-    private int carCount;
-    private int rentedCars;
-    private int servicedCars;
-    private int availableCars;
-    private int clientCount;
-    private int officeCount;
-    private int employeeCount;
+    private long carCount;
+    private long rentedCars;
+    private long servicedCars;
+    private long availableCars;
+    private long clientCount;
+    private long officeCount;
+    private long employeeCount;
     public StatisticsDTO(){
 
     }
 
-    public int getCarCount() {
+    public long getCarCount() {
         return carCount;
     }
 
-    public void setCarCount(int carCount) {
+    public void setCarCount(long carCount) {
         this.carCount = carCount;
     }
 
-    public int getRentedCars() {
+    public long getRentedCars() {
         return rentedCars;
     }
 
-    public void setRentedCars(int rentedCars) {
+    public void setRentedCars(long rentedCars) {
         this.rentedCars = rentedCars;
     }
 
-    public int getServicedCars() {
+    public long getServicedCars() {
         return servicedCars;
     }
 
-    public void setServicedCars(int servicedCars) {
+    public void setServicedCars(long servicedCars) {
         this.servicedCars = servicedCars;
     }
 
-    public int getAvailableCars() {
+    public long getAvailableCars() {
         return availableCars;
     }
 
-    public void setAvailableCars(int availableCars) {
+    public void setAvailableCars(long availableCars) {
         this.availableCars = availableCars;
     }
 
-    public int getClientCount() {
+    public long getClientCount() {
         return clientCount;
     }
 
-    public void setClientCount(int clientCount) {
+    public void setClientCount(long clientCount) {
         this.clientCount = clientCount;
     }
 
-    public int getOfficeCount() {
+    public long getOfficeCount() {
         return officeCount;
     }
 
-    public void setOfficeCount(int officeCount) {
+    public void setOfficeCount(long officeCount) {
         this.officeCount = officeCount;
     }
 
-    public int getEmployeeCount() {
+    public long getEmployeeCount() {
         return employeeCount;
     }
 
-    public void setEmployeeCount(int employeeCount) {
+    public void setEmployeeCount(long employeeCount) {
         this.employeeCount = employeeCount;
     }
 }
--- src/main/java/shop/dziupla/spring/login/security/services/StatisticsService.java
+++ src/main/java/shop/dziupla/spring/login/security/services/StatisticsService.java
@@ -1,13 +1,28 @@
 package shop.dziupla.spring.login.security.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import shop.dziupla.spring.login.payload.response.StatisticsDTO;
+import shop.dziupla.spring.login.repository.CarRepository;
+import shop.dziupla.spring.login.repository.EmployeeRepository;
+import shop.dziupla.spring.login.repository.OfficeRepository;
 
 @Service
 public class StatisticsService {
-
+    @Autowired
+    CarRepository carRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    OfficeRepository officeRepository;
     public StatisticsDTO getStatistics(){
         var result = new StatisticsDTO();
-        return null;
+        result.setCarCount(carRepository.count());
+        result.setAvailableCars(carRepository.countAllByAvailable(true));
+        result.setServicedCars(carRepository.countAllByTechnicalStatus(false));
+        result.setRentedCars(result.getCarCount() - result.getAvailableCars() - result.getServicedCars());
+        result.setEmployeeCount(employeeRepository.count());
+        result.setOfficeCount(officeRepository.count());
+        return result;
     }
 }
